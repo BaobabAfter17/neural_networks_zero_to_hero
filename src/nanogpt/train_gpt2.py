aae7f407706fad9f717a794c66b4f725310e602d
Yakun Yao <yakunyao@gmail.com>
2026-07-16 21:51:27 +0900

    nanogpt: Section 1: data loader lite

--- src/nanogpt/train_gpt2.py
+++ src/nanogpt/train_gpt2.py
@@ -169,6 +169,34 @@ class GPT(nn.Module):
         
         return model
 
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+    
+        with open('../../data/input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        # state
+        self.current_position = 0
+    
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position + B * T + 1]
+        x = (buf[:-1]).view(B, T)   # inputs
+        y = (buf[1:]).view(B, T)    # targets
+        # advance position in the tensor
+        self.current_position += B * T
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + (B * T) + 1 > len(self.tokens):
+            self.current_position = 0
+        return x, y
+
 # -------------------------------------------------
 # attempt to autodetect the device
 devide = "cpu"
@@ -178,33 +206,21 @@ elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"using device: {device}")
 
-# get a data batch
-enc = tiktoken.get_encoding('gpt2')
-with open('../../data/input.txt', 'r') as f:
-    text = f.read()
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T + 1])
-buf = buf.to(device)
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
+train_loader = DataLoaderLite(B=4, T=32)
 
-# get logits
-# model = GPT.from_pretrained('gpt2')
 model = GPT(GPTConfig())
 model.to(device)
-# logits, loss = model(x, y)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
     print(f"step {i}, loss: {loss.item()}")
 
-print(loss)
 import sys; sys.exit(0)
 
 num_return_sequences = 5
